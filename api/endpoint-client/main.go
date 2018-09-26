6051bdec539941beae5ca6574b32526b24efd450
Ribal Aladeeb <aladeeb.r@hotmail.com>
2018-09-26 01:00:04 -0400

    Create a dummy endpoint into the backend
    For now the server doesn't do much, but at least it can accept grpc
    client connections. Added an endpoint server and client

---
+++ api/endpoint-client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"SOEN341-Project/api/platform"
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+const address = "localhost:9090"
+
+func main() {
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect %v", err)
+	}
+	defer conn.Close()
+	client := platform.NewPlatformClient(conn)
+	status, err := client.AddClass(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("success: %v\nmessage: %v\n", status.Success, status.Message)
+}
