5b80c0f817e6ed859fff2e0d040d4e3f8136edc1
DPigeon <D_Pig@encs.concordia.ca>
2018-10-03 00:45:58 -0400

    Fixes
    Rearranged somethings

--- login/functions.js
+++ login/functions.js
@@ -21,45 +21,21 @@ function validateForm() {
 	}
 }
 			
-$("#button1").on("click", function(s) { //If the login button is clicked
-	s.preventDefault(); //prevents from opening a link
-	//Variables
-	var log = $("#log");
-	$(".form_error").css("display", "none");
-	var login = {user: $.trim(log.find("#uname").val()), pass: $.trim(log.find("#password").val()),}; //Gets the username and password from login page for endpoint
-	if ((login.user === '') && (login.pass === '')) { //if it is blank
-		/*Show messages below the login button*/
-		$('#userError').text("Username must not be blank").show(); 
-		$('#passError').text("Password must not be blank").show();
-	} else if (login.pass === '') {
-		$('#passError').text("Password cannot be blank").show();
-	} else if (login.user === '') {
-		$('#userError').text("Username cannot be blank").show();
-	} else {
-		$('#userError').text("Enter a valid username").show();
-	}
-	
-	/*For backend request*/
-	var backend = JSON.stringify(login);
-	$.ajax({ type: "POST", // endpoint
-	dataType: "json", crossDomain: true, data: backend,}).done(function(backend) { //For the backend request part
-	}).fail(function(xhr) {
-	document.getElementById("button1").disabled = false;
-	$("#button1").html("Login");
-	$(".alert").css("display", "block"); //Shows the CSS needed for the block alert
-	})
-});
+
 			
 $(function() { //Function for the remember me button checkbox, uses localStorage from browser
 	if (localStorage.chkbx && localStorage.chkbx != '') {
 		$('#rememberMe').attr('checked', 'checked');
+		$('#uname').val(localStorage.usrname);
 		$('#password').val(localStorage.pass);
 	} else {
 		$('#rememberMe').removeAttr('checked');
+		$('#uname').val('');
 		$('#password').val('');
 	}
 	$('#rememberMe').click(function() { //If the button rememberMe is clicked, do the function above
 		if ($('#rememberMe').is(':checked')) { 
+			localStorage.usrname = $('#uname').val();
 			localStorage.pass = $('#password').val(); //Stores infos
 			localStorage.chkbx = $('#rememberMe').val(); //Stores infos
 		} else {
@@ -69,6 +45,7 @@ $(function() { //Function for the remember me button checkbox, uses localStorage
 			localStorage.chkbx = '';
 		}
 	});
+});
 	
   	//caps lock verification function
 	function isCapsLockOn(e) {
@@ -87,4 +64,3 @@ $(function() { //Function for the remember me button checkbox, uses localStorage
 		e = e || window.event;
 		showCapsLockMsg(e);
 	}
-});
