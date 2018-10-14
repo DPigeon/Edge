c6ae71a55d9b348866e0fe0ccd6408324cba00e3
David <D_Pig@encs.concordia.ca>
2018-10-14 16:22:48 -0400

    New component
    Created a new component to show the message

--- messages/src/App.js
+++ messages/src/App.js
@@ -1,14 +1,16 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import Reply from "./components/reply.jsx";
 import Messager from "./components/messager.jsx";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return ( <
-      Reply / >
+    return (
+      <React.Fragment>
+        <main className="container" />
+        <Messager />
+        <Reply />
+      </React.Fragment>
     );
   }
 }
--- messages/src/components/messager.jsx
+++ messages/src/components/messager.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
 
 class Messager extends Component {
-  state = {};
+  state = {
+    to: "",
+    from: "Andrew",
+    message: ""
+  };
+
+  componentDidMount() {
+    //Where we call our requests
+    /*fetch('<your-heroku-url-goes-here>')
+    .then(results => {
+        return results.json();*/
+  }
+
   render() {
-    return <h6>Message Here</h6>;
+    return (
+      <div>
+        <div className="panel panel-default messager-editor">
+          <div className="card-title">Message from {this.state.from}</div>
+          <div className="panel" />
+        </div>
+      </div>
+    );
   }
 }
 
