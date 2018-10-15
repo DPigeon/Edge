faa91c9567f4a73bc33d036db20cd9d3637cfa47
David <D_Pig@encs.concordia.ca>
2018-10-15 00:33:51 -0400

    JSON test and some updates on the messges
    JSON file to request data from a rest API JSON server to test.

--- messages/src/App.js
+++ messages/src/App.js
@@ -1,18 +1,47 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import Reply from "./components/reply.jsx";
 import Messager from "./components/messager.jsx";
 import "./App.css";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() { //request data from  server
+    fetch('http://localhost:3001/messages')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+
+      });
+  }
+
   render() {
-    return (
-      <React.Fragment>
-        <main className="container" />
-        <Messager />
-        <Reply />
-      </React.Fragment>
+
+    return ( <
+      React.Fragment >
+      <
+      main className = "container" / >
+      <
+      Messager / >
+      <
+      Reply / >
+      <
+      /React.Fragment>
     );
   }
 }
+
 
 export default App;
--- messages/src/components/messager.jsx
+++ messages/src/components/messager.jsx
@@ -1,44 +1,52 @@
 import React, { Component } from "react";
 
 class Messager extends Component {
-  /*state = {
-    from: "Andrew",
-    message:
-      "Hey, how are you ? I wanted to know if I could borrow your calculator for the midterm tomorrow. Thank you ! Now, let me test your message output: ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
-  };*/
-
   constructor(props) {
     super(props);
 
     this.state = {
-      from: "Andrew",
-      messages: ["Hello World!"],
-      message:
-        "Hey, how are you ? I wanted to know if I could borrow your calculator for the midterm tomorrow. Thank you ! Now, let me test your message output: ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+      items: [],
+      isLoaded: false
     };
   }
 
   componentDidMount() {
-    //Where we call our requests
-    /*fetch('<your-heroku-url-goes-here>')
-    .then(results => {
-        return results.json();*/
+    fetch("http://localhost:3001/messages")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
   }
 
   render() {
-    return (
-      <div>
-        <div className="navbar navbar-toggler badge-dark navbar-collapse">
-          Inbox Messages
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div> Loading, please wait... </div>;
+    } else {
+      //Data has been loaded
+      return (
+        <div>
+          <div className="navbar navbar-toggler badge-dark navbar-collapse">
+            Inbox Messages
+          </div>
+          <ul>
+            {items.map(item => (
+              <div className="card messager-editor messager-body">
+                <li key={item.id}>
+                  <h3>
+                    <div className="card-title">Message from {item.from}</div>
+                  </h3>
+                  <div className="card-block">{item.msg}</div>
+                </li>
+              </div>
+            ))}
+          </ul>
         </div>
-        <div className="card messager-editor messager-body">
-          <h3>
-            <div className="card-title">Message from {this.state.from}</div>
-          </h3>
-          <div className="card-block">{this.state.message}</div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
