0df9cc5271167ac1a219ac9b2fbc7ff0ff99f8b7
Ribal Aladeeb <aladeeb.r@hotmail.com>
2018-10-16 15:27:16 -0400

    Implement the Signup feature on the backend
    requesting with POST at /signup and sending a json object of the
    format defined in User.js will result in the user being saved in the
    hosted dabatase. The endpoint returns a json of the form:
    {
      success: true/false
      message: string // if success == true then check message
    }

--- backend/Controller.js
+++ backend/Controller.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const User = require('./User')
+const Persistence = require('./Persistence')
 
 // ============ Allow Requests from a Browser ==========
 app.use(bodyParser.json()); // for parsing application/json
@@ -21,11 +23,24 @@ app.get('/', (req, res) => {
     console.log("Get request at path '/'")
 })
 
+app.post('/signup', (req,res)=>{
+    const{firstname,lastname,email,password,isteacher} = req.body
+    const user = new User({firstname,lastname,email,password,isteacher})
+    console.log('Attemp at signup:\n',);
+    console.log(user);
+    const {success, message} = Persistence.RegisterUser(user)
+    const status = {success,message}
+    console.log(status);
+    res.json(status)
+    res.end()
+})
+
 app.post('/login',(req,res)=>{
     const{firstname,lastname,email,password, isteacher} = req.body
-    console.log({firstname,lastname,email,password, isteacher} )
+    const user = new User({firstname,lastname,email,password, isteacher})
+    console.log(user)
     res.contentType('application/json')
-    res.json({firstname,lastname,email,password, isteacher})
+    res.send(Persistence.queryStringify(user))
 })
 
 
--- backend/Persistence.js
+++ backend/Persistence.js
@@ -1,8 +1,9 @@
 const User = require('./User')
 const mysql = require('sync-mysql')
+
 // ----------------------------------------------------------------------------------------
 // PLEASE DO NOT TOUCH THESE CONSTANTS
-// THEY USE TO CONNECT TO THE HOSTED DATABASE
+// THEY ARE USED TO CONNECT TO THE HOSTED DATABASE
 const dbIp = '18.221.83.136'
 const dbRootUser = 'root'
 const dbUser = 'soen341'
@@ -12,10 +13,33 @@ const dbUserPass = 'ilovedocker'
 
 const dbConn = new mysql({
     host: dbIp,
-    user: dbUser,
-    password: dbUserPass,
+    user: dbRootUser,
+    password: dbRootPass,
     database: dbName
 })
-class Persistence {
 
+// ----------------------------------------------------------------------------------------
+
+class Persistence {
+    static RegisterUser(user) {
+        // Try to enter a new record in the database.
+        try {
+            const queryResult = dbConn.query(
+                `INSERT INTO user VALUES( ${this.queryStringify(user)} )`)
+            console.log(queryResult);
+        } catch (error) {
+            console.log("error => \n"+error);
+            console.log("error.code => \n"+error.code);
+            return { success: false, message: error.code }
+        }
+        return { success: true }
+    }
+
+    // Return a string representing the query format of the object's values.
+    // This string should be passed in the values() of an sql query.
+    static queryStringify(anyObject) {
+        return Object.values(anyObject).map(x => "'" + x + "'").join(',');
+    }
 }
+
+module.exports = Persistence
