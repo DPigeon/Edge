686f94a13e44cf1a51760d9a0107ad7d7ce86eac
DPigeon (David) <37888675+DPigeon@users.noreply.github.com>
2018-10-31 00:40:42 -0400

    Update profile.jsx
    Fixing conflicts

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -1,88 +1,67 @@
-import React, { Component } from "react";
-import AuthService from "./login/authService";
-import "./css/profile.css";
-
-const Auth = new AuthService("http://localhost:3001");
-
-export default class Profile extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    email: localStorage.getItem("email"),
-    password: ""
-  };
-
-  componentDidMount() {
-    if (!Auth.loggedIn()) {
-      //if the user not logged in
-      this.props.history.replace("/login"); //go login
-    }
-  }
-
-  change = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-  onSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    console.log(this.state);
-  };
-
-  onEdit = e => {
-    this.setState({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: ""
-    });
-    //e.preventDefault();
-    //this.props.onEdit(this.state);
-    //console.log(this.state);
-  };
-
-  render() {
+render() {
     return (
-      <div className="profile">
-        <form>
-          <input
-            className=""
-            name="firstName"
-            placeholder="First Name"
-            value={this.state.firstName}
-            onChange={e => this.change(e)}
-          />
+      <React.Fragment>
+        <div className="profilecontainer">
+          <img
+            src={require("./images/banner.jpg")}
+            alt="Welcome"
+            className="banner"
+			/>
+          <center>
+            {" "}
+            <img
+              src={require("./images/profile.png")}
+              alt="profile"
+              className="pp"
+            />
+          </center>
+          <h3>Name Name</h3>
+          <button className="editpic">Update Info</button>
           <br />
-          <input
-            className=""
-            name="lastName"
-            placeholder="Last Name"
-            value={this.state.lastName}
-            onChange={e => this.change(e)}
-          />
           <br />
-          <input
-            className=""
-            name="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={e => this.change(e)}
-          />
           <br />
-          <input
-            className=""
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={e => this.change(e)}
-          />
           <br />
-          <button onClick={e => this.onEdit(e)}> Edit </button>
-          <button onClick={e => this.onSubmit(e)}> Save </button>
-        </form>
-      </div>
+        </div>
+         <div className="profile">
+          <form>
+            <input
+              className=""
+              name="firstName"
+              placeholder="First Name"
+              value={this.state.firstName}
+              onChange={e => this.change(e)}
+            />
+            <br />
+            <input
+              className=""
+              name="lastName"
+              placeholder="Last Name"
+              value={this.state.lastName}
+              onChange={e => this.change(e)}
+            />
+            <br />
+            <input
+              className=""
+              name="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={e => this.change(e)}
+            />
+            <br />
+            <input
+              className=""
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={e => this.change(e)}
+            />
+            <br />
+            <button onClick={e => this.onEdit(e)}> Edit </button>
+            <button onClick={e => this.onSubmit(e)}> Save </button>
+          </form>
+        </div>
+      </React.Fragment>
     );
   }
 }
