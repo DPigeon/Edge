9f7a910ef006e2cc03050bd11c5d3048e875824a
DPigeon <D_Pig@encs.concordia.ca>
2018-11-06 17:32:04 -0500

    Enhanced the Logic of Notifications
    - Created a method to show the notifications.
    - Notification can either be a post or message notification
    - Trying to find a way to implement the notifications by getting them and posting them

--- frontend/src/components/notifications/notify.jsx
+++ frontend/src/components/notifications/notify.jsx
@@ -5,39 +5,61 @@ class Notify extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showPopup: false,
-      notificationCounter: 3
+      notificationCounter: 3,
+      notifications: []
     };
   }
 
-  //
+  componentDidMount() {
+    fetch("http://localhost:3001/notifications")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          notifications: json
+        });
+      });
+  }
 
-  showPopupHandler = event => {
-    event.preventDefault();
-    this.setState({ showPopup: !this.state.showPopup });
-  };
-
-  showNotifications() {
-    //To do later, just a quick preview was done.
+  showNotifications(type, name) {
+    //either of type message or post
+    this.setState({
+      notificationCounter: this.state.notificationCounter + 1
+    });
+    switch (type) {
+      case "message":
+        return (
+          <a className="dropdown-item" href="/threads">
+            Message from {name}
+          </a>
+        );
+      case "post":
+        return (
+          <a className="dropdown-item" href="/">
+            Post from {name}
+          </a>
+        );
+      default:
+        break;
+    }
   }
 
   render() {
     //we can map an array of notifications coming from the backend so that we see all of them on hover
     //this is a simple notification example
     return (
-      <span class="navbar-text float-xs-right ml-auto">
-        <div class="dropdown">
-          <button class="btn btn-success dropdown-toggle dropbtn">
+      <span className="navbar-text float-xs-right ml-auto">
+        <div className="dropdown">
+          <button className="btn btn-success dropdown-toggle dropbtn">
             {this.state.notificationCounter}
           </button>
-          <div class="dropdown-content">
-            <a class="dropdown-item" href="/threads">
+          <div className="dropdown-content">
+            <a className="dropdown-item" href="/threads">
               Message from David
             </a>
-            <a class="dropdown-item" href="/">
+            <a className="dropdown-item" href="/">
               New post from Ruslan
             </a>
-            <a class="dropdown-item" href="/threads">
+            <a className="dropdown-item" href="/threads">
               Message from Maria
             </a>
           </div>
