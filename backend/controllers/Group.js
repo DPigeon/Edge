bc03b5fdc560e2cead3ebc19e8af374cc1fc90d8
AnasBuyumad <anas.buyumad@epitech.eu>
2018-11-12 03:38:42 -0500

    feat(groups/requests): Group request can be created and processed
    A request can be created with a POST on /groups/:group_id/requests
    The list of requests for a group can be retrieved with a GET on /groups/:group_id/requests
    
    A request can be individually retrieved with a GET on /groupRequests/:requestId
    A request can be processed with a POST on /groupRequests/:requestId
    
    Issues #95 #96 #97
    
    For details on the request processing, see issue #97

--- backend/controllers/Group.js
+++ backend/controllers/Group.js
@@ -74,6 +74,67 @@ class GroupController {
 
         res.json(query.members);
     }
+
+    static createRequest(req, res) {
+        if (!req.params.groupId) {
+            return res.status(400).json({error: "Please provide a valid group id in the route"});
+        }
+
+        if (!req.body.user_id) {
+            return res.status(400).json({error: "Please provide valid data"});
+        }
+
+        let query = Group.createRequest(req.params.groupId, req.body.user_id);
+
+        if (!query.success) {
+            return res.send(query.error);
+        }
+
+        res.json(query.request);
+    }
+
+    static getRequest(req, res) {
+        if (!req.params.requestId) {
+            return res.status(400).json({error: "Please provide a valid request id in the route"});
+        }
+
+        let query = Group.getRequest(req.params.requestId);
+
+        if (!query.success) {
+            return res.send(query.error);
+        }
+
+        res.json(query.request);
+    }
+
+    static getRequests(req, res) {
+        if (!req.params.groupId) {
+            return res.status(400).json({error: "Please provide a valid group id in the route"});
+        }
+
+        let query = Group.getRequests(req.params.groupId);
+
+        if (!query.success) {
+            return res.send(query.error);
+        }
+
+        res.json(query.requests);
+    }
+
+    static processRequest(req, res) {
+        if (!req.params.requestId) {
+            return res.status(400).json({error: "Please provide a valid request id in the route"});
+        }
+
+        let query = Group.processRequest(req.params.requestId, req.body.accept);
+
+        if (!query.success) {
+            return res.send(query.error);
+        }
+
+        res.json({success: true});
+    }
+
 }
 
 module.exports = GroupController;
--- backend/models/Group.js
+++ backend/models/Group.js
@@ -92,7 +92,7 @@ class Group {
         console.log('Getting all members for group with id : ', groupId);
 
         try {
-            const queryResult = db.SyncConn.query(`SELECT * FROM user_groups WHERE group_id = ${groupId}`)
+            const queryResult = db.SyncConn.query(`SELECT * FROM user_groups WHERE group_id = ${groupId}`);
             queryResult.forEach((association) => {
                 members.push({user_id: association.user_id, admin: !!association.admin});
             })
@@ -105,6 +105,100 @@ class Group {
         console.log(members);
         console.log('---------------------------------------');
         return { success: true, members: members };
+    }
+
+    static createRequest(groupId, userId) {
+
+        let request = { user_id: userId, group_id: groupId };
+
+        console.log('Creating an addition request for user of id : ', userId);
+        console.log('Into group of id : ', groupId);
+
+        try {
+            const queryResult = db.SyncConn.query(`INSERT INTO group_requests (user_id, group_id) VALUES('${userId}', '${groupId}')`);
+            request.id = queryResult.insertId;
+        } catch (error) {
+            console.log('Error : ', error);
+            console.log('Error code : ', error.code);
+            return { success: false, error: error };
+        }
+
+        console.log(request);
+        console.log('---------------------------------------');
+        return { success: true, request: request };
+    }
+
+    static getRequest(requestId) {
+
+        let request = null;
+
+        console.log('Getting request with id : ', requestId);
+
+        try {
+            const queryResult = db.SyncConn.query(`SELECT * FROM group_requests WHERE id = ${requestId}`);
+            request = queryResult[0];
+        } catch (error) {
+            console.log('Error : ', error);
+            console.log('Error code : ', error.code);
+            return { success: false, error: error };
+        }
+
+        console.log(request);
+        console.log('---------------------------------------');
+        return { success: true, request: request };
+    }
+
+    static getRequests(groupId) {
+
+        let requests = null;
+
+        console.log('Getting all requests for group with id : ', groupId);
+
+        try {
+            const queryResult = db.SyncConn.query(`SELECT * FROM group_requests WHERE group_id = ${groupId}`);
+            requests = queryResult;
+        } catch (error) {
+            console.log('Error : ', error);
+            console.log('Error code : ', error.code);
+            return { success: false, error: error };
+        }
+
+        console.log(requests);
+        console.log('---------------------------------------');
+        return { success: true, requests: requests };
+    }
+
+    static processRequest(requestId, accept) {
+
+        let requestQuery = this.getRequest(requestId);
+        let request = requestQuery.request;
+
+        if (!requestQuery.success) {
+            return requestQuery;
+        }
+
+        console.log('Processing request with id : ', requestId);
+        console.log('With acceptance : ', accept);
+
+        try {
+
+            if (accept) {
+                let memberRequest = this.addMember(request.group_id, request.user_id, false);
+                if (!memberRequest.success) {
+                    return memberRequest;
+                }
+            }
+
+            const queryResult = db.SyncConn.query(`DELETE FROM group_requests WHERE id = ${requestId}`);
+
+        } catch (error) {
+            console.log('Error : ', error);
+            console.log('Error code : ', error.code);
+            return { success: false, error: error };
+        }
+
+        console.log('---------------------------------------');
+        return { success: true }
     }
 }
 
