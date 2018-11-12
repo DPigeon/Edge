d66bf2fb5237610507933ceac14c497d033e625a
DPigeon <D_Pig@encs.concordia.ca>
2018-11-11 20:25:53 -0500

    Fixed some bugs on Group List
    - Testing with localhost:3001 (no groups are implemented on backend for now so it was tested with a tested backend on port 3001)
    - Must create a join group function
    - Must create a create group function

--- frontend/src/components/groups/groupList.jsx
+++ frontend/src/components/groups/groupList.jsx
@@ -1,111 +1,96 @@
 import React, { Component } from "react";
-
-
+import IndividualGroup from "./individualGroup";
 
 class GroupList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            groupName: "",
-            groupDescritpion: "",
-            groupList: [],
-            gourpTitle: "",
-            isLoaded: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      groupName: "",
+      groupDescritpion: "",
+      groupList: [],
+      groupTitle: "",
+      groupAdmin: "",
+      isLoaded: false
+    };
+  }
 
-
-        };
+  componentDidMount() {
+    let jwt = localStorage.getItem("jwt");
+    if (jwt === undefined || jwt === null) {
+      //if the user not logged in
+      this.props.history.replace("/login"); //go login
     }
-
-    componentDidMount() {
-        let jwt = localStorage.getItem("jwt");
-        if (jwt === undefined || jwt === null) {
-            //if the user not logged in
-            this.props.history.replace("/login"); //go login
-        }
-        fetch("http://localhost:8000/groups", {
-            method: "GET"
-        })
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    isLoaded: true,
-                    groupList: json
-                });
-            });
-    }
-
-    handleClickItem(gN, gD, gT) {
+    fetch("http://localhost:3001/groups", {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(json => {
         this.setState({
-            groupName: gN,
-            groupDescritpion: gD,
-            groupTitle: gT
-
+          isLoaded: true,
+          groupList: json
         });
-    }
+      });
+  }
 
+  createGroup(groupAdmin) {} // Need to work on this
 
-    groupList() {
-        var { isLoaded, groups } = this.state;
-        if (!isLoaded) {
-            return <div> Loading the groups, please wait... </div>;
-        } else {
-            return (
-                <div>
-                    <div>
-                        {groups.map(item => (
-                            <div>
-                                <h10>
-                                    <div key={item.gN}>
-                                        <ul>
-                                            <li>
-                                                <button
-                                                    onClick={() =>
-                                                        this.individualGroup()
-                                                    }
-                                                >
-                                                    {item.groupName}
-                                                    <br />
-                                                    Group Description {item.groupDescritpion}
-                                                    <br />
-                                                    Group Title {item.groupTitle}
-                                                    {/* <br/>
-                          <button onclick = {() => this.individualGroup()}>
-                           Join
-                          </button> */}
-                                                </button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </h10>
-                            </div>
-                        ))}
-                        <center>
-                            <button
-                                className="newGroup"
-                                onClick={() => this.creatGroup()}
-                            >
-                                Creat Group
+  joinGroup() {} // Need to work on this.
+
+  handleClickItem(gN, gD, gT) {
+    this.setState({
+      groupName: gN,
+      groupDescritpion: gD,
+      groupTitle: gT
+    });
+  }
+
+  groupList() {
+    var { isLoaded, groupList } = this.state;
+    if (!isLoaded) {
+      return <div> Loading the groups, please wait... </div>;
+    } else {
+      return (
+        <div>
+          <div>
+            {groupList.map(item => (
+              <div>
+                <h10>
+                  <div key={item.id}>
+                    <ul>
+                      <li>
+                        <a href={"/groups/" + item.id}>
+                          {item.name}
+                          <br />
+                          Group Title: {item.title}
+                          <br />
+                          Group Description: {item.description}
+                        </a>
+                        <button onclick={() => this.joinGroup()}>Join</button>
+                      </li>
+                    </ul>
+                  </div>
+                </h10>
+              </div>
+            ))}
+            <center>
+              <button className="newGroup" onClick={() => this.creatGroup()}>
+                Create Group
               </button>
-                        </center>
-                    </div>
-                </div>
-            );
-        }
+            </center>
+          </div>
+        </div>
+      );
     }
+  }
 
-
-
-
-    render() {
-        return (
-            //should separate each group to different buttons     
-            <React.Fragment>
-                <div className="groupList">
-                    {this.groupList}
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      //should separate each group to different buttons
+      <React.Fragment>
+        <div className="groupList">{this.groupList()}</div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default GroupList;
