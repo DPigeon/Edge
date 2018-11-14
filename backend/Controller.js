5ccfc90b1156f09db55c4408faaa8b46d9fe15fc
DPigeon <D_Pig@encs.concordia.ca>
2018-11-14 17:53:57 -0500

    Update Controller.js
    Getting ready to to work on notifications on the backend

--- backend/Controller.js
+++ backend/Controller.js
@@ -1,106 +1,106 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const User = require('./models/User')
-const UserController = require('./controllers/User')
-const Auth = require('./Auth')
-
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+const UserController = require("./controllers/User");
+const Auth = require("./Auth");
 
 // ============ Allow Requests from a Browser ==========
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next()
-})
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
-app.get('/', (req, res) => {
-    res.status(200)
-    res.contentType('application/json')
+app.get("/", (req, res) => {
+    res.status(200);
+    res.contentType("application/json");
     res.json({
-        response: 'hello world',
+        response: "hello world",
         method: "GET"
-    })
-    console.log("Get request at path '/'")
-})
+    });
+    console.log("Get request at path '/'");
+});
 
-app.post('/signup', (req, res) => {
-    const { firstname, lastname, email, password, is_teacher } = req.body
-    const user = new User({ firstname, lastname, email, password, is_teacher })
-    console.log('Attempt at signup:\n');
+app.post("/signup", (req, res) => {
+    const { firstname, lastname, email, password, is_teacher } = req.body;
+    const user = new User({ firstname, lastname, email, password, is_teacher });
+    console.log("Attempt at signup:\n");
     console.log(user);
-    const { success, message } = UserController.RegisterUser(user)
-    const status = { success, message }
+    const { success, message } = UserController.RegisterUser(user);
+    const status = { success, message };
     console.log(status);
-    res.json(status)
-    res.end()
-})
+    res.json(status);
+    res.end();
+});
 
-app.post('/login', (req, res) => {
-    const { email, password } = req.body
-    const result = Auth.AuthenticateUser(email, password)
-    console.log("Attempt at login:", { email, password })
-    res.json(result)
-})
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const result = Auth.AuthenticateUser(email, password);
+    console.log("Attempt at login:", { email, password });
+    res.json(result);
+});
 
-const MessageController = require('./controllers/Message');
+const MessageController = require("./controllers/Message");
 
-app.post('/messages', MessageController.create);
-app.get('/messages/:messageId', MessageController.getById);
+app.post("/messages", MessageController.create);
+app.get("/messages/:messageId", MessageController.getById);
 //app.put('/messages/:messageId', messageController.updateById); NOT NEEDED FOR NOW
 //app.delete('/messages/:messageId', messageController.deleteById); NOT NEEDED FOR NOW
 
-const ThreadController = require('./controllers/Thread');
+const ThreadController = require("./controllers/Thread");
 
-app.get('/threads', ThreadController.getAll);
-app.post('/threads', ThreadController.create);
-app.get('/threads/:threadId', ThreadController.getById);
+app.get("/threads", ThreadController.getAll);
+app.post("/threads", ThreadController.create);
+app.get("/threads/:threadId", ThreadController.getById);
 //app.put('/threads/:threadId', threadController.updateById); NOT NEEDED FOR NOW
-app.get('/threads/:threadId/messages', ThreadController.getAllMessagesById);
+app.get("/threads/:threadId/messages", ThreadController.getAllMessagesById);
 
-const GroupController = require('./controllers/Group');
+const GroupController = require("./controllers/Group");
 
-app.get('/groups', GroupController.getAll);
-app.post('/groups', GroupController.create);
-app.get('/groups/:groupId', GroupController.getById);
+app.get("/groups", GroupController.getAll);
+app.post("/groups", GroupController.create);
+app.get("/groups/:groupId", GroupController.getById);
 
-app.post('/groups/:groupId/members', GroupController.addMemberToGroup);
-app.get('/groups/:groupId/members', GroupController.getMembers);
+app.post("/groups/:groupId/members", GroupController.addMemberToGroup);
+app.get("/groups/:groupId/members", GroupController.getMembers);
 
-app.post('/groups/:groupId/requests', GroupController.createRequest);
-app.get('/groups/:groupId/requests', GroupController.getRequests);
+app.post("/groups/:groupId/requests", GroupController.createRequest);
+app.get("/groups/:groupId/requests", GroupController.getRequests);
 
-app.get('/groupRequests/:requestId', GroupController.getRequest);
-app.post('/groupRequests/:requestId', GroupController.processRequest);
+app.get("/groupRequests/:requestId", GroupController.getRequest);
+app.post("/groupRequests/:requestId", GroupController.processRequest);
 
-const PostController = require('./controllers/Post')
+//app.get("/notifications/:notificationId", GroupController.getNotifs);
+//app.post("/notifications/:notificationId", GroupController.processNotifs);
+
+const PostController = require("./controllers/Post");
 
 // req.body  = {author_email, data, group_id}
-app.post('/posts', PostController.create)
+app.post("/posts", PostController.create);
 // gets all post
-app.get('/posts', PostController.retrieveAll)
+app.get("/posts", PostController.retrieveAll);
 // gets all posts written by one user in the shared dashboard
-app.get('/posts/:author_email,', PostController.retrieveByUser)
-
-
-
-
+app.get("/posts/:author_email,", PostController.retrieveByUser);
 
 // modify the test db only
 // ------------------------------------------------------------------
-app.post('/test/posts', PostController.create)
+app.post("/test/posts", PostController.create);
 
-app.get('/test/posts', PostController.retrieveAll)
+app.get("/test/posts", PostController.retrieveAll);
 
 // gets all posts written by one user in the shared dashboard
-app.get('/test/posts/:author_email', PostController.retrieveByUser)
+app.get("/test/posts/:author_email", PostController.retrieveByUser);
 // ------------------------------------------------------------------
-
 
 let port = 8000;
 const api = app.listen(port, () => {
-    console.log('backend started on port', port)
+    console.log("backend started on port", port);
 });
 
 module.exports = api;
