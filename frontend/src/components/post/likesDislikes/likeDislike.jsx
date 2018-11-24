4ea88a2dc51968c83a3f65f05d122cc0f0f567b6
DPigeon <D_Pig@encs.concordia.ca>
2018-11-24 11:46:37 -0500

    Likes/Dislikes Update
    - Can POST likes/dislikes now (no duplicate as well done on backend before)
    - Slightly beginning to enhance the feature

--- frontend/src/components/post/likesDislikes/likeDislike.jsx
+++ frontend/src/components/post/likesDislikes/likeDislike.jsx
@@ -13,11 +13,13 @@ class LikeDislike extends Component {
       counterDislike: 0,
       isLikeButtonDisabled: false,
       isDislikeButtonDisabled: false,
-      items: []
+      items: [],
+      arrayEmails: []
     };
   }
 
   handleLike = (email, postId) => {
+    console.log(email, postId); //gets the post id
     if (this.state.liked === true) {
       //if the post is liked by you, do not add a new like, delete it
       /*fetch("http://localhost:8000/likes", {
@@ -112,7 +114,7 @@ class LikeDislike extends Component {
   }
 
   getLengthOfDislikes(postId) {
-    var length = 0;
+    var length = this.state.counterDislike;
     for (var i = 0; i < this.props.dislikes.length; i++) {
       if (postId === this.props.dislikes[i].post_id) {
         length = length + 1; //calculates the length every time it sees a post with same post id
@@ -121,17 +123,38 @@ class LikeDislike extends Component {
     }
   }
 
-  getWhoLikedByIdByPostId(postId) {
+  setWhoLikedByPostId(postId) {
+    var emails = [];
     for (var i = 0; i < this.props.likes.length; i++) {
-      if (postId === this.props.likes[i].post_id)
-        return this.props.likes[i].email;
+      if (postId === this.props.likes[i].post_id) {
+        emails = this.props.likes[i].email;
+        this.setState({ arrayEmails: emails });
+      }
     }
+    //this.setState({ arrayEmails: emails });
+    //console.log(this.state.arrayEmails);
+    return this.state.arrayEmails;
   }
 
   getWhoDislikedByPostId(postId) {
     for (var i = 0; i < this.props.dislikes.length; i++) {
       if (postId === this.props.dislikes[i].post_id)
         return this.props.dislikes[i].email;
+    }
+  }
+
+  getLikes(email, postId) {
+    //must show your linked/disliked and the length on them
+    for (var i = 0; i < this.props.likes.length; i++) {
+      if (postId === this.props.likes[i].post_id)
+        return this.props.likes[i].email;
+    }
+  }
+
+  getDislikes(email, postId) {
+    for (var i = 0; i < this.props.likes.length; i++) {
+      if (postId === this.props.likes[i].post_id)
+        return this.props.likes[i].email;
     }
   }
 
@@ -148,6 +171,18 @@ class LikeDislike extends Component {
       >
         <div className="editcontainer">
           {this.getLengthOfLikes(this.props.postId)} people liked this post
+          {this.setWhoLikedByPostId(this.props.postId).map(item => (
+            <div className="Like List">
+              <li key={item.id}>
+                <div className="itemLike">
+                  {item}
+                  <h5>
+                    <a href={`/users/` + item}>{item}</a>
+                  </h5>
+                </div>
+              </li>
+            </div>
+          ))}
         </div>
       </Popup>
     );
