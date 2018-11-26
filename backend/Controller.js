2d7d548344c278589662f71ae04c4aad00e8ba26
Ribal Aladeeb <aladeeb.r@hotmail.com>
2018-11-26 15:23:48 -0500

    Refactor the image related code
    Refactor the code related to image uploading in Controller.js to
    controllers/images.js

--- backend/Controller.js
+++ backend/Controller.js
@@ -154,73 +154,8 @@ app.post('/test/likes', LikeController.create)
 
 
 // uploading Images
-const multer = require('multer')
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, './images/')
-    },
-    filename: (req, file, callback) => {
-        const generateFileName = (file) => {
-            let startingPoint = file.originalname.lastIndexOf('.')
-            let fileType = file.originalname.slice(startingPoint)
-            return Date.now() + fileType
-        }
-        callback(null, generateFileName(file))
-    }
-})
-
-const ImageUploader = multer({
-    storage: storage,
-    fileFilter: (req, file, callback) => {
-        const t = file.mimetype
-        if (t == 'image/jpeg' || t == 'image/jpg' || t == 'image/png') {
-            callback(null, true)
-        } else {
-            const errMsg =
-                'wrong myme type, you are trying to upload ' +
-                'a file format other than jpeg or png'
-
-            callback(new Error(errMsg), false)
-        }
-    }
-})
-
-const singleImageUpload = ImageUploader.single('myimage')
-
-app.post("/images", (req, res) => {
-
-    return singleImageUpload(req, res, (err) => {
-        if (err instanceof multer.MulterError) {
-            console.log('multer err =>', err)
-            return res.status(400).json({
-                success: false,
-                message: err.Error
-            })
-        }
-        else if (err) {
-            console.log('err =>', err)
-            return res.status(400).json({
-                success: false,
-                message: err.message
-            })
-        } else if (req.file == undefined) {
-            return res.status(400).json({
-                success: false,
-                message: 'You did not send an image'
-            })
-        } else {
-            console.log('file:', req.file)
-
-            return res.status(200).json({
-                success: true,
-                message: 'file was successfully saved',
-                filename: req.file.filename
-            })
-        }
-    })
-
-
-})
+const ImageController = require('./controllers/Images')
+app.post("/images", ImageController.saveImage)
 
 module.exports.determineTestAndAuth = (req) => {
     let test = false
--- backend/controllers/Images.js
+++ backend/controllers/Images.js
@@ -1,4 +1,60 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, './images/')
+    },
+    filename: (req, file, callback) => {
+        const generateFileName = (file) => {
+            let startingPoint = file.originalname.lastIndexOf('.')
+            let fileType = file.originalname.slice(startingPoint)
+            return Date.now() + fileType
+        }
+        callback(null, generateFileName(file))
+    }
+})
+
+const ImageUploader = multer({
+    storage: storage,
+    fileFilter: (req, file, callback) => {
+        const t = file.mimetype
+        if (t == 'image/jpeg' || t == 'image/jpg' || t == 'image/png') {
+            callback(null, true)
+        } else {
+            const errMsg =
+                'wrong myme type, you are trying to upload ' +
+                'a file format other than jpeg or png'
+
+            callback(new Error(errMsg), false)
+        }
+    }
+})
+
+const singleImageUpload = ImageUploader.single('myimage')
 
 
+module.exports.saveImage = (req, res) => {
+    return singleImageUpload(req, res, (err) => {
 
-module.exports.SaveImage()
+        if (err) {
+            console.log('err =>', err)
+            return res.status(400).json({
+                success: false,
+                message: err.message
+            })
+        } else if (req.file == undefined) {
+            return res.status(400).json({
+                success: false,
+                message: 'You did not send an image'
+            })
+        } else {
+            console.log('file:', req.file)
+
+            return res.status(200).json({
+                success: true,
+                message: 'file was successfully saved',
+                filename: req.file.filename
+            })
+        }
+    })
+}
