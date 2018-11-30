8b50e5ee140fbc6762ff3a2df0fa76fe6192d427
DPigeon <D_Pig@encs.concordia.ca>
2018-11-30 02:13:54 -0500

    Tag Feature without Notifications
    - You can now tag people on posts only
    - You do not receive a notification yet
    - Tag with @.
    - We see the HTML in posts
    - HTML is now enabled in posts.

--- frontend/src/components/post/post.jsx
+++ frontend/src/components/post/post.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
 import CommentDisplay from "./comments/commentDisplay";
 import LikeDislike from "./likesDislikes/likeDislike";
+import Parser from "html-react-parser";
 import "./styles/post.css";
 
 class Post extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { postBody: "" };
+  }
+
+  replaceAt(string, index, replace) {
+    return string.substring(0, index) + replace + string.substring(index + 1);
+  }
+
+  findAtIndexInPosts() {
+    var string = this.props.postBody;
+    var at = string.indexOf("@");
+    console.log(at);
+    return at;
+  }
+
+  replaceByMention() {
+    if (this.props.postBody.includes("@") === true) {
+      return Parser(
+        this.replaceAt(
+          this.props.postBody,
+          this.findAtIndexInPosts(),
+          `<a href="">[@]</a>`
+        )
+      );
+    } else {
+      return <div>{Parser(this.props.postBody)}</div>;
+    }
   }
 
   render() {
@@ -16,8 +42,7 @@ class Post extends Component {
           <h6>
             Post by <a href={"/user/" + this.props.by}>{this.props.by}</a>
           </h6>
-
-          {this.props.postBody}
+          {this.replaceByMention()}
         </div>
         <LikeDislike
           id={this.props.id}
